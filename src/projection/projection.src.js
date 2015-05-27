2cf57dbe8b89a08837af00480a1895fb4df97cc1
hxg <hiroki.gota@spatialvision.com.au>
2015-05-27 11:56:05 +1000

    changed to angular module

--- src/projection/projection.src.js
+++ src/projection/projection.src.js
@@ -1,18 +1,9 @@
 'use strict';
-///**
-// * @since 0.0.1
-// * @copyright 2015 Spatial Vision, Inc. http://spatialvision.com.au
-// * @license The MIT License
-// * @author Spatial Vision
-// * @version 0.1.0
-// */
-//
-
 angular.module('farmbuild.webmapping')
   .factory('webMappingProjections',
   function ($log, farmdata) {
     var webMappingProjections = {supported:farmbuild.farmdata.crsSupported}
-    farmbuild.farmdata.crsSupported.forEach(function(crs) {
+    farmdata.crsSupported.forEach(function(crs) {
       proj4.defs(crs.name, crs.projection);
     });
     return webMappingProjections;
